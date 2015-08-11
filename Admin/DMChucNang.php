ff78299216d7af945fda41ee2297da13c15b0fc3
milomoli2134 <milomoli2134@gmail.com>
2015-08-11 18:51:30 +0700

    xumaili
    sửa lại tí

--- Admin/DMChucNang.php
+++ Admin/DMChucNang.php
@@ -26,6 +26,12 @@
 
     <link rel="stylesheet" type="text/css" href="../css/style.css" media="screen" />
 
+        <link rel="stylesheet" type="text/css" href="../css/demo.css" />
+        <link rel="stylesheet" type="text/css" href="../css/style6.css" />
+        <link href='http://fonts.googleapis.com/css?family=Terminal+Dosis' rel='stylesheet' type='text/css' />
+    
+    
+    
 </head>
 <body style="background-color: lightgrey;min-height:100%;">
 	<?php
@@ -35,27 +41,42 @@
 	<div class="row" style="background-color: whitesmoke;padding-top: 5px;">
     	<p class="bg-primary" style="margin-right: 5px;margin-left: 5px;font-size: 30px;color:white;font-family: tahoma;text-align: center;border-radius:5px;padding-bottom: 5px;"> <b>Danh mục chức năng</b> </p>
     </div>
-    <div class="row">
-    	<div class="col-xs-4" align="center">
-            <a href="QLBaiViet.php">
-                <img class="thumbnail" src="../button/button1.gif" style="height:72px;width:180px;margin-top:5px;margin-bottom:0px;">
-
-            </a>
-        </div>
-        <div class="col-xs-4" align="center">
-            <a href="QLThuCung.php">
-                <img class="thumbnail" src="../button/button2.gif" style="height:72px;width:180px;margin-top:5px;margin-bottom:0px;">
-
-            </a>
-        </div>
-        <div class="col-xs-4" align="center">
-            <a href="QLTaiKhoan.php">
-                <img class="thumbnail" src="../button/button3.gif" style="height:72px;width:180px;margin-top:5px;margin-bottom:0px;">
-
-            </a>
-        </div>
+    <div class="row" style="height:230px;">
+    	 <div class="content">
+<ul class="ca-menu">
+                    <li style="margin-left: 100px; border-radius:20px;">
+                        <a href="QLBaiViet.php">
+                            <img src="../button/b2.gif" class="ca-icon" style="margin-left:55px;">
+                            <div class="ca-content">
+                                <h2 class="ca-main">Quản trị bài viết</h2>
+                                
+                            </div>
+                        </a>
+                    </li>
+                    <li style="margin-left: 80px; border-radius:20px;">
+                        <a href="QLThuCung.php">
+                            <img src="../button/b1.gif" class="ca-icon">
+                            <div class="ca-content">
+                                <h2 class="ca-main">Quản trị thú cưng</h2>
+                                
+                            </div>
+                        </a>
+                    </li>
+                    <li style="margin-left: 80px; border-radius:20px;">
+                        <a href="QLTaiKhoan.php">
+                            <img src="../button/b3.gif" class="ca-icon">
+                            <div class="ca-content">
+                                <h2 class="ca-main">Quản trị User</h2>
+                                
+                            </div>
+                        </a>
+                    </li>
+                    
+                </ul>
+            </div><!-- content -->
     </div>
     
 </div>
+<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.6.4/jquery.min.js"></script>
 </body>
 </html>
