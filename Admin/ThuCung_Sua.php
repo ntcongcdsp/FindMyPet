9923db3c1815a784c05b76a71633b67a7ed65f1f
milomoli2134 <milomoli2134@gmail.com>
2015-08-13 19:22:42 +0700

    a
    a

--- Admin/ThuCung_Sua.php
+++ Admin/ThuCung_Sua.php
@@ -76,8 +76,8 @@
 			<div class="form-group">
 				<label for="Loai" class="col-sm-2 control-label">Loài: </label>
 				<div class="col-sm-5">
-      				<input type="text" class="form-control" name="Loai" placeholder="Loài" value="<?php echo $row['Loai']; ?>"
-                                </div></div>
+      				<input type="text" class="form-control" name="Loai" placeholder="Loài" value="<?php echo $row['Loai']; ?>">
+                                </div>
 			</div>
             <div class="form-group">
 				<label for="Giong" class="col-sm-2 control-label">Giống: </label>
